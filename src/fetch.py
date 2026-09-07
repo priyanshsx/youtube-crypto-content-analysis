4b533bff66ac267a4a342ccb3feffd7bf6677243
Priyansh <saxenapriyansh95@gmail.com>
2026-09-07 19:11:40 +0530

    added the video id sourcer function

--- src/fetch.py
+++ src/fetch.py
@@ -36,3 +36,23 @@ VIDEOS_PER_CHANNEL = 50
 
 # building the get videos function 
 
+def get_video_ids(channel_id, max_results=50):
+    video_ids = []
+    next_page_token = None # since youtube paginates results 
+
+    while len(video_ids) < max_results: 
+        request = youtube.search().list(
+            part='id',
+            channelId=channel_id,
+            order='date',
+            type='video',
+            maxResults=min(50, max_results - len(video_ids)),
+            pageToken=next_page_token
+        )
+        response = request.execute()
+        for item in response('items'):
+            video_ids.append(item['id']['videoId'])
+        next_page_token = response.get('nextPageToken')
+        if not next_page_token: 
+            break
+    return video_ids
