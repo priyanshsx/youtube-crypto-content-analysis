635f6db8affc16b398df6b4509806cfe6f31759c
Priyansh <saxenapriyansh95@gmail.com>
2026-09-07 18:34:18 +0530

    added the channel IDs

--- src/script.py
+++ src/script.py
@@ -4,4 +4,9 @@ import os
 load_dotenv() 
 API_KEY = os.environ.get('YOUTUBE_API_KEY')
 
-channel_list = []
+# channel IDs
+# anthony pompliano: UCML9PlpcOxM_H53IM0fa4XA; the moon show: UCkIcpHGyZtp9Cdyoh5pL3Jg; moneyzg: UCIEvlRpHBVFthrF6pZzBEXw;
+# cryptosrus: UCIEvlRpHBVFthrF6pZzBEXw; virtualbacon:UCIEvlRpHBVFthrF6pZzBEXw; benjamin cowen: UCRvqjQPSeaWn-uEx-w0XOIg;
+# cryptobanter: UCybasP-2D2b5kTLAb_kvhWQ; discover crypto: UCB8sMtMOYVY_m6jYZcnQdUA; altcoin daily: UC7KjtEJT6HvI3kBcF2I4vXg;
+# coinbureau: UCqK_GSMbpiV8spgD3ZGloSw;
+
