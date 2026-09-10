d2fd17eedf8978399b830a62472532bb4f38a80d
Priyansh <saxenapriyansh95@gmail.com>
2026-09-10 15:54:04 +0530

    cleaned up the sql script

--- sql/sqlscript.sql
+++ sql/sqlscript.sql
@@ -8,12 +8,3 @@ SELECT channel_name, COUNT(*) AS video_count FROM youtube_videos
 GROUP BY channel_name 
 ORDER BY total_videos 
 
--- building the analysis columns
-CREATE TABLE videos_categories AS SELECT *, 
-    (like_count + comment_count) * 1.0 / NULLIF(view_count,0) AS engagement_rate,
-CASE 
-    WHEN duration_seconds < 600 THEN 'short'
-    WHEN duration_seconds < 1200 THEN 'medium'
-    ELSE 'long'
-END AS length_bucket 
-FROM youtube_videos 
