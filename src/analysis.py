7615c35968c544bd9ff5a7568fbc3bcf81d2f6f7
Priyansh <saxenapriyansh95@gmail.com>
2026-09-11 12:43:44 +0530

    formatted the analysis script

--- src/analysis.py
+++ src/analysis.py
@@ -16,17 +16,17 @@ df = con.sql("""
 # recall ques 1: does video length correlate with views/engagement rate? 
 
 filtered_df = df.groupby('video_length_category')[['view_count', 'engagement_rate', 'duration_seconds']].median()
-# print(f"Median values:\n {filtered_df}")
+print(f"Median values:\n {filtered_df}")
 
 corr, p_value = stats.pearsonr(df['duration_seconds'], df['engagement_rate'])
-# print(f"Correlation: {corr:.4f}")
-# print(f"p-value: {p_value: .4f}")
+print(f"Correlation: {corr:.4f}")
+print(f"p-value: {p_value: .4f}")
 
 # recall question 2: do price-prediction titles, news-reaction titles, or 
 # educational titles perform differently? 
 
 title_analysis_df = df.groupby('title_category')[['view_count', 'engagement_rate']].median()
-# print(f"Title Analysis Median Values\n {title_analysis_df}")
+print(f"Title Analysis Median Values\n {title_analysis_df}")
 
 # exporting to csv for vizzing on tableau
 df.to_csv('/home/priyansh/Documents/d/youtube crypto content analysis/data/processed/youtube_analysis_master.csv', index=False)
