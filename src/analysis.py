60d6e3f2c3e50b3735d2bd425ef202544bd7e738
Priyansh <saxenapriyansh95@gmail.com>
2026-09-11 12:16:55 +0530

    analyzed question 1

--- src/analysis.py
+++ src/analysis.py
@@ -2,12 +2,22 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import duckdb 
+from scipy import stats 
+import statsmodels.api as sm 
 
-con = duckdb.connect('home/priyansh/Documents/d/youtube crypto content analysis/db/main.duckb')
+# duckdb for importing tables
+con = duckdb.connect('/home/priyansh/Documents/d/youtube crypto content analysis/db/main.duckdb')
 
+# creating the pandas df 
 df = con.sql("""
-    SELECT 
-        channel_name, title, duration_seconds, view_count, comment_count, engagement_rate, video_length_category, title_category
-        FROM question_two
+    SELECT * FROM question_two
 """).df()
 
+# recall ques 1: does video length correlate with views/engagement rate? 
+
+filtered_df = df.groupby('video_length_category')[['view_count', 'engagement_rate', 'duration_seconds']].median()
+print(f"Median values: {filtered_df}")
+
+corr, p_value = stats.pearsonr(df['duration_seconds'], df['engagement_rate'])
+print(f"Correlation: {corr:.4f}")
+print(f"p-value: {p_value: .4f}")
